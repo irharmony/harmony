63e7c18f5fe79f4588636f54762ce9273572bd9d
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-24 20:28:57 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES] })
 
-const { token } = require("./data/config.json")
+const { token, Prefix, Emotes, ChannelsID } = require("./data/config.json")
 client.login(token)
 
 const fs = require('fs')
 
-// fs.readdir("./commands", (err, files) => {
-//     if (err) return console.log("Could not find any commands!")
-//     const jsFiles = files.filter(f => f.split(".").pop() === "js")
-//     if (jsFiles.length <= 0) return console.log("Could not find any commands!")
-//     jsFiles.forEach(file => {
-//         const cmd = require(`./commands/${file}`)
-//         console.log(`Loaded ${file}`)
-//         client.commands.set(cmd.name, cmd)
-//         if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
-//     })
-// })
+const embedmsg = new Discord.MessageEmbed()
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
 
+console.log('------------- LOADING COMMANDS -------------');
+fs.readdir("./commands", (err, files) => {
+    if (err) return console.log("Could not find any commands!")
+    const jsFiles = files.filter(f => f.split(".").pop() === "js")
+    if (jsFiles.length <= 0) return console.log("Could not find any commands!")
+    jsFiles.forEach(file => {
+        const cmd = require(`./commands/${file}`)
+        console.log(`Loaded ${file}`)
+        client.commands.set(cmd.name, cmd)
+        if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
+    })
+})
+
+console.log('-------------- LOADING EVENTS --------------');
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
@@ -29,31 +35,27 @@ for (const file of eventFiles) {
     console.log((event.name + ' LOADED'));
 }
 
-// client.on("messageCreate", async message => {
-//     if (message.author.bot || !message.guild) return
-//     const prefix = client.config.prefix
-//     if (!message.content.startsWith(prefix)) return
-//     const args = message.content.slice(prefix.length).trim().split(/ +/g)
-//     const command = args.shift().toLowerCase()
-//     const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+client.on("messageCreate", async message => {
+    if (message.author.bot || !message.guild) return
+    if (!message.content.startsWith(Prefix)) return
+    const args = message.content.slice(Prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
 
-//     if (!cmd) return
-//     if (cmd.inVoiceChannel && !message.member.voice.channel) {
-//         embedmsg.setTitle(`${client.emotes.error} | You must be in a voice channel!`)
-//         embedmsg.setColor('#f67975')
-//         return message.channel.send({ embeds: [embedmsg] })
-//     }
-//     try {
-//         cmd.run(client, message, args)
-//     } catch (e) {
-//         embedmsg.setTitle(`${client.emotes.error} | Error:`)
-//         embedmsg.setColor('YELLOW')
-//         embedmsg.setDescription('```js\n' + e + '\n```')
-//         embedmsg.setTimestamp()
-//         client.channels.cache.get(client.config.log).send({ embeds: [embedmsg] })
-//         message.channel.send(`${client.emotes.error} | SomeThing Went Wrong`)
-//     }
-// })
+    if (!cmd) return
+
+    try {
+        cmd.run(client, message, args)
+    } catch (e) {
+        embedmsg.setTitle(`${Emotes.Error} | Error:`)
+        embedmsg.setColor('YELLOW')
+        embedmsg.setDescription('```js\n' + e + '\n```')
+        embedmsg.setTimestamp()
+        client.channels.cache.get(ChannelsID.ErrLog).send({ embeds: [embedmsg] })
+        message.channel.send({ embeds: [embedmsg] })
+    }
+})
+
 
 process.on('unhandledRejection', err => {
     console.log(err);
