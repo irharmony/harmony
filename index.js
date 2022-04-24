a4f2e64b406184c0ff942579bf21edefe0071735
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-24 23:16:47 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES] })
 
-const { token, Prefix, Emotes, ChannelsID } = require("./data/config.json")
+const { token } = require("./data/config.json")
 client.login(token)
 
-const fs = require('fs')
+const chalk = require('chalk')
 
-console.log('-------------- LOADING EVENTS --------------');
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const { readdirSync } = require('fs')
+client.commands = new Discord.Collection();
+
+const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(chalk.red('------------- LOADING COMMANDS -------------'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+    console.log(chalk.blue(command.data.name + ' LOADED'));
+}
+console.log(chalk.red('-------------- LOADING EVENTS --------------'));
+const eventFiles = readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     if (event.once) {
@@ -15,9 +25,8 @@ for (const file of eventFiles) {
     } else {
         client.on(event.name, (...args) => event.execute(...args));
     }
-    console.log((event.name + ' LOADED'));
+    console.log(chalk.blue(event.name + ' LOADED'));
 }
-
 
 process.on('unhandledRejection', err => {
     console.log(err);
