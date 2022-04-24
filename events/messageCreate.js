c8b8783972123b72dbd0afbc609ec8c9ec030d98
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-24 23:57:23 +0430

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js")
-const { Prefix, Emotes, OWNER,ChannelsID } = require("../data/config.json")
+const { Prefix, Emotes, OWNER, ChannelsID } = require("../data/config.json")
 const { inspect } = require('util');
 
 module.exports = {
     name: 'messageCreate',
-   async execute(message) {
+    async execute(message, client) {
         if (!message.content.startsWith(Prefix) || message.author.bot) return;
 
         const args = message.content.slice(Prefix.length).trim().split(' ');
@@ -36,7 +36,29 @@ module.exports = {
 
         if (command === 'verify' || command === 'new') {
             message.reply(`${Emotes.Tick} | درخواست شما ثبت شد لطفا در ویس های موجود منتظر ادمین باشید`)
-            return client.channels.cache.get(ChannelsID.VerifyID).send(`یک کاربر در ویس منتظر ادمین هست\nUserTag : ${message.author.tag}\n Mention : <@${message.author.id}>`)
+            return message.guild.channels.cache.get(ChannelsID.VerifyID).send(`یک کاربر در ویس منتظر ادمین هست\nUserTag : ${message.author.tag}\nMention : <@${message.author.id}>`)
+        }
+
+        if (command === 'bug') {
+            if (message.channel.id === ChannelsID.BugChannel) {
+                if (!args.length) return;
+                message.react('✅')
+                const bugembed = new Discord.MessageEmbed()
+                    .setColor('RED')
+                    .setTitle('باگ گزارش شده توسط : ' + message.author.username)
+                    .setThumbnail(message.author.displayAvatarURL({ size: 2048, dynamic: true }))
+                    .setDescription(args.join(" "))
+                    .setTimestamp()
+                message.guild.channels.cache.get('901665723552788521').send('<@' + message.author.id + '>', { embed: bugembed })
+                    .then(embedMessage => {
+                        embedMessage.react('👍');
+                        embedMessage.react('👎');
+                    })
+            }
+            else {
+                message.reply(`این دستور تنها در چنل <#${ChannelsID.BugChannel}> دردسترس است`).then(message.react('❌'));
+            }
+
         }
     },
 };
