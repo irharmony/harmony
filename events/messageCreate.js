d92df2d1fe23daa08d526c49f5420e1f1afb2676
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 00:26:31 +0430

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
-const { Prefix, Emotes, OWNER, ChannelsID } = require("../data/config.json")
+const { Prefix, Emotes, OWNER, ChannelsID, ServerBanRole } = require("../data/config.json")
+const reasons = require('../data/reason.json')
 const { inspect } = require('util');
+const { readFile } = require('fs')
 
 module.exports = {
     name: 'messageCreate',
@@ -60,6 +62,156 @@ module.exports = {
             }
         }
 
+
+        if (command === 'ban') {
+            if (message.member.roles.cache.find(r => r.id === '930971681441325117') || message.member.roles.cache.find(r => r.id === '899049712294965329')) {
+                let BanRole = message.guild.roles.cache.get(ServerBanRole);
+                let BanUser = message.mentions.members.first();
+                if (!BanUser) return message.reply("Please Mention Someone Else!").then((declineMsg) => {
+
+                    declineMsg.delete({ timeout: 5000 });
+                });
+
+                if (!args[1]) {
+                    readFile('./data/reason.json', 'utf8', function (err, contents) {
+                        const banhelptable = new Discord.MessageEmbed()
+                            .setColor('RED')
+                            .setTitle('لطفا یکی از متغییر ها را تعریف کنید')
+                            .setDescription('```json\n' + contents + '\n```')
+                        message.reply({ embeds: [banhelptable] })
+                    })
+                }
+
+                if (args[1] === '1') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.one, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.one)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+
+                if (args[1] === '2') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.two, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.two)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+                if (args[1] === '3') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.three, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.three)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+                if (args[1] === '4') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.four, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.four)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+                if (args[1] === '5') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.five, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.five)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+                if (args[1] === '6') {
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: reasons.six, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(reasons.six)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+
+
+                if (args[1] === '7') {
+
+                    const logmsg = new Discord.MessageEmbed()
+                        .setColor('GREEN')
+                        .addFields(
+                            { name: 'توسط', value: `<@${message.author.id}>`, inline: true },
+                            { name: 'به', value: `${BanUser}`, inline: true },
+                            { name: 'به دلیله', value: res.other, inline: false },
+                            { name: 'توضیحات اضافه', value: args.join(" "), inline: false },
+                        )
+
+                    message.guild.channels.cache.get('899050010950389800').send({ embeds: [logmsg] })
+                    BanUser.roles.add(BanRole);
+                    BanUser.setNickname(res.other)
+                    message.reply(`Added the role **Server Ban** to ${BanUser} by <@${message.author.id}>.`)
+                }
+            }
+        }
+
+
+        if (message.content.startsWith(`${Prefix}unban`)) {
+
+            if (message.member.roles.cache.find(r => r.id === '930971681441325117') || message.member.roles.cache.find(r => r.id === '899049712294965329')) {
+                let BanRole = message.guild.roles.cache.get(ServerBanRole);
+                let BanUser = message.mentions.members.first();
+                if (!BanUser) return message.reply("Please Mention Someone Else!").then((declineMsg) => { declineMsg.delete({ timeout: 5000 }); });
+
+                BanUser.roles.remove(BanRole);
+                BanUser.setNickname(BanUser.user.username)
+                message.reply(`Removed the role **Server Ban** from ${BanUser} by <@${message.author.id}>.`)
+            }
+        }
 
         // ------------------------------------
 
