7ae411fa9f97de6dd098ae3d95f886c5156f80ad
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-24 18:51:42 +0430

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,9 @@
 const voiceDiscord = require(`@discordjs/voice`)
-const {Guild,
-ChannelID} = require("../data/config.json")
+const { Guild, ChannelsID } = require("../data/config.json")
+const fs = require('fs')
+
+const { Database } = require('beta.db')
+const db = new Database('./data/mic.json')
 
 module.exports = {
     name: 'ready',
@@ -17,32 +20,52 @@ module.exports = {
             }]
         });
 
+        // -------------------- VOICE CONNECTION --------------------
         const connection = voiceDiscord.joinVoiceChannel({
-            channelId: ChannelID,
+            channelId: ChannelsID.VC,
             guildId: Guild,
             adapterCreator: SERVER.voiceAdapterCreator,
             selfDeaf: true,
         });
         const player = voiceDiscord.createAudioPlayer();
         connection.subscribe(player);
+
+        // setInterval(async () => {
+            const server = client.guilds.cache.get(Guild)
+
+            var date_channel = client.channels.cache.get(ChannelsID.DATE)
+            var member_channel = client.channels.cache.get(ChannelsID.MEMBER)
+            // -------------------- VOICE CONNECTION --------------------
+
+            const voiceChannels = server.channels.cache.filter(c => c.type === 'voice');
+
+            let alivecount = 0;
+            for (const [id, voiceChannel] of voiceChannels) alivecount += voiceChannel.members.size;
+
+            let totalmـ = JSON.parse(fs.readFileSync("./mic.json"));
+            let totalm = totalmـ.TocalMIC.tm;
+            let date_ = totalmـ.TocalMIC.date;
+            let date_now = moment2().format('jYYYY/jM/jD');
+
+
+
+            let now_db = JSON.parse(
+                fs.readFileSync("./mic.json")
+            );
+
+
+
+            if (now_db.TocalMIC.date == moment2().format('jYYYY/jM/jD')) {
+                if (totalm < alivecount) {
+                    var t = { date: date_, tm: alivecount }
+                    db.set('TocalMIC', t);
+                }
+                date_channel.setName('┣︳' + date_)
+            } else {
+                var t_ = { date: date_now, tm: alivecount }
+                db.set('TocalMIC', t_);
+                date_channel.setName('┣︳' + date_now)
+            }
+        // }, 60000);
     },
 };
-
-
-
-
-
-
-
-// player.on(voiceDiscord.AudioPlayerStatus.Idle, () => {
-//     const EMBED = new MessageEmbed()
-//         .setTitle('There is currently no resource for the player to be playing (Trying to replay the resource)')
-//         .setThumbnail(client.user.displayAvatarURL({ format: 'png', size: 2048 }))
-//         .setDescription(`Powered By : [${name}](${homepage})`)
-//         .setTimestamp()
-
-//     player.play(resource);
-//     connection.subscribe(player);
-//     client.channels.cache.get(logID).send({ embeds: [EMBED] });
-// });
-
