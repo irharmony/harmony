aa55e4cff556f9873cdfed11ebb733f412667c54
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-24 19:34:44 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES] })
 
-const config = require("./data/config.json")
-client.login(config.token)
+const {token} = require("./data/config.json")
+client.login(token)
 
 const fs = require('fs')
 
