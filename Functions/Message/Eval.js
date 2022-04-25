156f72761e1e43ead6891c5e50b269eb54b71a63
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 09:22:43 +0430

    Create Eval.js

---
+++ Functions/Message/Eval.js
@@ -0,0 +1,23 @@
+module.exports = async ({Discord, message, args, OWNER, inspect }) => {
+    if (OWNER.includes(message.author.id)) {
+        if (!args.length) return;
+        let evaled;
+        try {
+            evaled = await eval(args.join(" "));
+            let evalend = new Discord.MessageEmbed()
+                .setTitle('Eval Result : ')
+                .setDescription('INPUT :\n```js\n' + args.join(" ") + '\n```\nOUTPUT :\n```js\n' + inspect(evaled) + '\n```')
+                .setColor("GREEN")
+            message.reply({ embeds: [evalend] })
+        }
+        catch (error) {
+            let evalerr = new Discord.MessageEmbed()
+                .setTitle('Thre Was An Error : ')
+                .setDescription('```js\n' + error + '```')
+                .setColor("RED")
+            message.reply({ embeds: [evalerr] })
+        }
+    } else {
+        return message.reply(Emotes.Error + ' You Dont Have Enough Permission');
+    }
+}
