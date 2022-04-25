25f68620d2465e5c4be25cfe929e8276f3206fd3
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 09:22:47 +0430

    Update handler.js

--- Functions/handler.js
+++ Functions/handler.js
@@ -2,3 +2,6 @@ exports.Voice = require('./Ready/Voice')
 exports.Data_Mic = require('./Ready/Data&Mic')
 exports.Banner = require('./Ready/Banner')
 exports.Presence = require('./Ready/Presence')
+
+exports.Eval = require('./Message/Eval')
+exports.New = require('./Message/New')
