f3382164e247748212205411e5287ce3317eb6d5
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 17:10:58 +0430

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,14 +7,14 @@ const functions = require('../Functions/handler')
 
 module.exports = {
     name: 'messageCreate',
-    async execute(message, client) {
+    async execute(message) {
         if (!message.content.startsWith(Prefix) || message.author.bot) return;
 
         const args = message.content.slice(Prefix.length).trim().split(' ');
         const command = args.shift().toLowerCase();
 
         if (command === 'eval') {
-            functions.Eval({ Discord, message, args, OWNER, inspect, Emotes, client })
+            functions.Eval({ Discord, message, args, OWNER, inspect, Emotes })
         }
 
         if (command === 'verify' || command === 'new') {
