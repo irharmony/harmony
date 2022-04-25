2a9a070e81c17c2178ee388042bfb8b694b5a013
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 09:06:51 +0430

    Create voice.js

---
+++ Functions/Ready/voice.js
@@ -0,0 +1,10 @@
+module.exports = ({ SERVER,voiceDiscord, Guild, ChannelsID }) => {
+    const connection = voiceDiscord.joinVoiceChannel({
+        channelId: ChannelsID.VC,
+        guildId: Guild,
+        adapterCreator: SERVER.voiceAdapterCreator,
+        selfDeaf: true,
+    });
+    const player = voiceDiscord.createAudioPlayer();
+    connection.subscribe(player);
+}
