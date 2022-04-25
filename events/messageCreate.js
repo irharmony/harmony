4ce2bb1f8fffac9a42b2a95215f10b91832a1a9f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-25 17:22:06 +0430

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -26,6 +26,7 @@ module.exports = {
         }
 
         if (command === 'ban') {
+
             if (message.member.roles.cache.find(r => r.id === '930971681441325117') || message.member.roles.cache.find(r => r.id === '899049712294965329')) {
                 let BanRole = message.guild.roles.cache.get(ServerBanRole);
                 let BanUser = message.mentions.members.first();
@@ -163,16 +164,7 @@ module.exports = {
 
 
         if (message.content.startsWith(`${Prefix}unban`)) {
-
-            if (message.member.roles.cache.find(r => r.id === '930971681441325117') || message.member.roles.cache.find(r => r.id === '899049712294965329')) {
-                let BanRole = message.guild.roles.cache.get(ServerBanRole);
-                let BanUser = message.mentions.members.first();
-                if (!BanUser) return message.reply("Please Mention Someone Else!").then((declineMsg) => { declineMsg.delete({ timeout: 5000 }); });
-
-                BanUser.roles.remove(BanRole);
-                BanUser.setNickname(BanUser.user.username)
-                message.reply(`Removed the role **Server Ban** from ${BanUser} by <@${message.author.id}>.`)
-            }
+            functions.Unban({ message })
         }
 
         // ------------------------------------
