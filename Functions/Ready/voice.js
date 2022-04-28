e382e1eab85bae900c0a0298804bb76bde1ab5f6
hesawmgh1098 <hesam.ghanbari1098@gmail.com>
2022-04-29 00:55:10 +0430

    Update voice.js

--- Functions/Ready/voice.js
+++ Functions/Ready/voice.js
@@ -1,10 +1,9 @@
 module.exports = ({ SERVER,voiceDiscord, Guild, ChannelsID }) => {
-    const connection = voiceDiscord.joinVoiceChannel({
-        channelId: ChannelsID.VC,
-        guildId: Guild,
-        adapterCreator: SERVER.voiceAdapterCreator,
-        selfDeaf: true,
-    });
-    const player = voiceDiscord.createAudioPlayer();
-    connection.subscribe(player);
+        const connection = voiceDiscord.joinVoiceChannel({
+            channelId: ChannelsID.VC,
+            guildId: 'Guild',
+            selfDeaf: true,
+            adapterCreator: client.guilds.cache.get(Guild).voiceAdapterCreator,
+ });
+
 }
