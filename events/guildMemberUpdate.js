35365453218525cdfa5c30d36f16fdf3c4c70b2b
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-30 18:16:16 +0430

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -21,40 +21,40 @@ module.exports = {
         let uqrole = oldMember.guild.roles.cache.get("899049713809100810") //@Quarantine Role
         if (oldMember.roles.cache.get(uqrole.id)) {
             // Whitelist Wick & (Developer & Mechanism) Role
-            if (QWL.includes(newMember.roles.cache.get)) {
-                let embed = new Discord.MessageEmbed()
-                    .setColor("#ce0000")
-                    .setAuthor(oldMember.user.username, oldMember.user.avatarURL({ dynamic: true }))
-                    .addFields({
-                        name: "User :",
-                        value: `╰୨ ${executorInGuild.user.username} (${executorInGuild.id})`,
-                        inline: false
-                    }, {
-                        name: "Q member :",
-                        value: `╰୨ ${oldMember.user.username} (${uqmember.user.id})`,
-                        inline: false
-                    },
-                        //  {
-                        //     name: "Role",
-                        //     value: `╰୨ ${difference} (${chnagerole.id})`,
-                        //     inline: false
-                        // }
-                    )
-                    .setThumbnail(oldMember.guild.iconURL({
-                        dynamic: true
-                    }))
-                    .setFooter(client.user.username, client.user.avatarURL())
-                let tarroles = newMember.roles.cache
-                let exroles = executorInGuild.roles.cache
-                client.channels.cache.get("Log Channel").send({ embeds: [embed] }).catch((e) => { //Log Channel
-                    console.log(e)
-                })
-                await executorInGuild.roles.remove(exroles)
-                await newMember.roles.remove(tarroles)
-                await executorInGuild.roles.add(uqrole)
-                await newMember.roles.add(uqrole)
-            } else {
-                return;
-            }
+            if (QWL.includes(newMember.roles.cache.get)) return;
+            let embed = new Discord.MessageEmbed()
+                .setColor("#ce0000")
+                .setAuthor(oldMember.user.username, oldMember.user.avatarURL({ dynamic: true }))
+                .addFields({
+                    name: "User :",
+                    value: `╰୨ ${executorInGuild.user.username} (${executorInGuild.id})`,
+                    inline: false
+                }, {
+                    name: "Q member :",
+                    value: `╰୨ ${oldMember.user.username} (${uqmember.user.id})`,
+                    inline: false
+                },
+                    //  {
+                    //     name: "Role",
+                    //     value: `╰୨ ${difference} (${chnagerole.id})`,
+                    //     inline: false
+                    // }
+                )
+                .setThumbnail(oldMember.guild.iconURL({
+                    dynamic: true
+                }))
+                .setFooter(client.user.username, client.user.avatarURL())
+            let tarroles = newMember.roles.cache
+            let exroles = executorInGuild.roles.cache
+            client.channels.cache.get("Log Channel").send({ embeds: [embed] }).catch((e) => { //Log Channel
+                console.log(e)
+            })
+            await executorInGuild.roles.remove(exroles)
+            await newMember.roles.remove(tarroles)
+            await executorInGuild.roles.add(uqrole)
+            await newMember.roles.add(uqrole)
+        } else {
+            return;
         }
     }
+}
