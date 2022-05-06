fec3e76e872961651f32884defe5425661caa5f9
unknown <azarihadi81@gmail.com>
2022-05-06 12:27:50 +0430

    Update Bax.js

--- Functions/Message/Bax.js
+++ Functions/Message/Bax.js
@@ -1,8 +1,5 @@
 module.exports = ({ message, Emotes, args, Discord }) => {
     if (!message.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) return message.reply(Emotes.Error + ' You Dont Have Enough Permission');
-    const name = args.join(" ")
-    if (!name) {
-        return message.reply(`لطفا اسم بکس رو وارد کنید !`);
-    }
+    if (!args.length) return message.reply(`لطفا اسم بکس رو وارد کنید !`);
 
 }
