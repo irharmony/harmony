e2ee32469be51ec7475bf602b34cbdfad75ca7cd
Mhmd_.akh <79569464+Mhmd-Akh@users.noreply.github.com>
2022-09-07 04:03:00 +0430

    Update handler.js

--- Functions/handler.js
+++ Functions/handler.js
@@ -1,6 +1,6 @@
 exports.Voice = require('./Ready/Voice')
 exports.Data_Mic = require('./Ready/Data&Mic')
-exports.Banner = require('./Ready/Banner')
+// exports.Banner = require('./Ready/Banner')
 exports.Presence = require('./Ready/Presence')
 
 exports.Eval = require('./Message/Eval')
